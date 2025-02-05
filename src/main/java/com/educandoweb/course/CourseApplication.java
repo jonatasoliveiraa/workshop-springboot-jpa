b1bcd7b093467fcfc82849ddccbacad021abaee7
Jonatas Oliveira <jonatasoliveiramont@gmail.com>
2025-02-05 19:03:07 -0300

    JPA repository, dependency injection, database seeding

--- src/main/java/com/educandoweb/course/CourseApplication.java
+++ src/main/java/com/educandoweb/course/CourseApplication.java
@@ -6,9 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CourseApplication {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		SpringApplication.run(CourseApplication.class, args);
-	}
+        SpringApplication.run(CourseApplication.class, args);
+
+    }
 
 }
--- src/main/java/com/educandoweb/course/resources/UserResource.java
+++ src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -11,8 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserResource {
 
     @GetMapping
-    public ResponseEntity<User> findAll(){
-        User u = new User(1, "Maria", "maria@gmail.com","40028922","123456");
+    public ResponseEntity<User> findAll() {
+        User u = new User(1, "Maria", "maria@gmail.com", "40028922", "123456");
         return ResponseEntity.ok().body(u);
     }
 }
